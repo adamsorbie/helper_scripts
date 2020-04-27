a0c556b8697799acf66c7c68aedeb8048c08285a
Adam <adam.sorbie@tum.de>
2020-04-27 13:44:41 +0200

    added support for phylo tree

--- import_phyloseq.R
+++ import_phyloseq.R
@@ -2,28 +2,12 @@
 #' Date: 2019-08-01
 library(tidyverse)
 library(phyloseq)
-library(zoo)
-
-# replaces name but needs to append unkown first
-
-#while(length(ind <- which(df$a == "")) > 0){
- # df$a[ind] <- df$a[ind -1]
-#}
-
-# can you look at na.locf function and basically make a version which adds unknown also ? 
-# needs to append unknown!
-append_nas <- function(taxa_split) {
-  taxa_nas <- taxa_split %>% mutate_all(na_if,"")
-  #taxa_out <- data.frame(t(apply(taxa_nas, 1, na.locf)))
-  return(taxa_nas)
-}
 
 split_taxonomy <- function(otu) {
   # select taxa column and split taxonomy column into separate columns using ; delimiter
    taxonomy_phylo <- select(otu, "taxonomy") %>%
     separate("taxonomy", c("Kingdom", "Phylum", "Class", "Order", "Family", "Genus"), sep = ";")
-   taxonomy_phylo_out <- append_nas(taxonomy_phylo)
-  return(taxonomy_phylo_out)
+  return(taxonomy_phylo)
 }
 
 read_tab_delim <- function(df) {
@@ -32,18 +16,24 @@ read_tab_delim <- function(df) {
   return(df_out)
 }
 
-load_phylo <- function(otu, taxa, mapping) {
-  # convert to phyloseq and return list
+load_phylo <- function(otu, taxa, mapping, tree=NULL) {
+  # convert to phyloseq and return list 
   phylo_otu <- otu_table(otu, taxa_are_rows = T)
-
+  
   phylo_tax <- tax_table(as.matrix(taxa))
-
-  phylo_map <- sample_data(mapping) # should re-write this to return phyloseq obj using merge_phyloseq
-
-  return(merge_phyloseq(phylo_otu, phylo_tax, phylo_map))
+  
+  phylo_map <- sample_data(mapping)
+  
+  if (exists("tree")){
+    phylo_tree <- read_tree(tree)
+    return(merge_phyloseq(phylo_otu, phylo_tax, phylo_tree, phylo_map))
+  }
+  else {
+    return(merge_phyloseq(phylo_otu, phylo_tax, phylo_map))
+  }
 }
 
-prep_4_phyloseq <- function(otu, mapping){
+prep_4_phyloseq <- function(otu, mapping, tree=NULL){
   # read files
   otu_taxa <- read_tab_delim(otu)
   metadata <- read_tab_delim(mapping)
@@ -51,10 +41,16 @@ prep_4_phyloseq <- function(otu, mapping){
   count_matrix <- as.matrix(subset(otu_taxa, select=-taxonomy))
   # split taxonomy
   taxonomy_phylo <- split_taxonomy(otu_taxa)
-  # make sure taxa table row names match otu
+  # make sure taxa table row names match otu 
   rownames(taxonomy_phylo) <- rownames(otu_taxa)
   # use load phylo function to convert all files to phyloseq objs
-  out <- load_phylo(count_matrix, taxonomy_phylo, metadata)
-
+  if (exists("tree")){
+    # if user wants to input a phylogenetic tree this code is called
+    out <- load_phylo(count_matrix, taxonomy_phylo, metadata, tree = tree)
+  }
+  else {
+  # without a tree
+    out <- load_phylo(count_matrix, taxonomy_phylo, metadata)
+  }
   return(out)
 }
